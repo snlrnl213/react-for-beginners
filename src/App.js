4ed2b486a8d007dd24839b97f6cf62de5b2f873d
wonsuk lee <snlrnl213@gmail.com>
2022-06-24 14:51:27 +0900

    22.06.24#5.3 effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import Button from "./Button"
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("")
+  const onClick = () => setValue((prev) => prev + 1)
+  const onChange = (event) => setKeyword(event.target.value)
+  console.log("i run all the time")
+  const iRunOnlyOnce = () => {
+    console.log("i run only once.")
+  }
+  useEffect(() => {
+    iRunOnlyOnce()
+  }, [])
+  useEffect(()=>{
+    if(keyword!=="" && keyword.length >5){
+      console.log("SEARCH FOR...",keyword)
+    }
+  },[keyword])
+  useEffect(()=>{
+    console.log("I run when 'counter' change.")
+  },[counter])
+  useEffect(()=>{
+    console.log("I run when 'counter','keyword' change.")
+  },[counter,keyword])
+  
+  
   return (
     <div>
       <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"}/>
+      <Button text={"Continue"} />
+      <hr />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>//리액트에서 제공하는 검사도구. 개발모드일때만 디버그를 하며 해당태그로 감싸져 있는 부분은 자손까지 검사(로그 두번찍힘;;)
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
